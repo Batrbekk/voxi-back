ee237c892a3f0a60e76831f7f635ff1032d5bbc7
batrbekk <batrbekk@gmail.com>
2025-11-20 13:44:02 +0500

    feat: add AI services test endpoints

--- src/modules/google-cloud/google-cloud.module.ts
+++ src/modules/google-cloud/google-cloud.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GoogleCloudService } from './google-cloud.service';
+import { GoogleCloudTestController } from './google-cloud-test.controller';
 
 @Module({
   imports: [ConfigModule],
+  controllers: [GoogleCloudTestController],
   providers: [GoogleCloudService],
   exports: [GoogleCloudService],
 })
